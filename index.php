bf75d42756566d27115c07f914123cebe34df009
towfiqpiash <towfiqpiash@gmail.com>
2014-07-13 09:40:08 +0600

    use GET method

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <body>
 
-	<form action= "index.php" method="POST">
+	<form action= "index.php" method="GET">
 		<input type ='text' name = 'idText'> <br/>
 		<input type ='text' name = 'nameText'> <br/>
 		<input type ='text' name = 'salaryText'> <br/>
@@ -11,14 +11,16 @@
 	</form>
 <?php
 	
-	if(isset($_POST['idText'])) //without value it does not execute
+	if(isset($_GET['idText'])) //without value it does not execute
 	{
-	$id = $_POST['idText'];
-	$name = $_POST['nameText'];
-	$salaryText = $_POST['salaryText'];
+	$id = $_GET['idText'];
+	$name = $_GET['nameText'];
+	$salaryText = $_GET['salaryText'];
 	
 	echo $id. ' ' . $name . ' ' . $salaryText;
 	}
-	
+
 
 ?>
+</body>
+</html>
