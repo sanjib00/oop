ef1b70c7c358aebe454adbd2546c6be2104e5e44
towfiqpiash <towfiqpiash@gmail.com>
2014-07-13 11:28:53 +0600

    get salary with bonus

--- employee.php
+++ employee.php
@@ -5,5 +5,27 @@ class Employee{
 	public $id;
 	public $name;
 	public $salary;
+	
+	public function get_salary_with_bonus()
+	{
+		if($this->salary>=30000)
+		
+			return $this->salary+$this->salary*0.2;
+			
+		
+		elseif($this->salary>=10000 && $this->salary<30000)
+		
+			return $this->salary+$this->salary*0.3;
+			
+		
+			
+		elseif($this->salary<10000)
+	
+			return $this->salary+$this->salary*0.5;
+			 
+		
+		
+		
+	}
 }
 ?>
